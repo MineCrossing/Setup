fa0457e6175beb34a5edd4c3df8e641c3cd1c4e2
TheMGRF <thomas.grf@googlemail.com>
2021-02-24 12:03:14 +0000

    Add linux install setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,18 @@
 echo "Starting MineCrossing setup!"
 
+echo "Starting basic package installation..."
+sudo apt update
+sudo apt upgrade
+sudo apt-get install nginx
+sudo apt-get install openjdk-11-jdk
+sudo apt install php php-cli php-fpm php-json php-common php-mysql php-zip php-gd php-mbstring php-curl php-xml php-pear php-bcmath
+sudo apt install mariadb-server
+sudo apt install redis-server
+sudo apt install nodejs
+sudo apt-get install npm
+echo "Base package intallation done!"
+
+echo "Starting to grab MineCrossing repositories from GitHub..."
 cd ..
 git clone https://github.com/MineCrossing/CoreUtilities.git && cd CoreUtilities/ && mvn install
 cd ..
@@ -13,5 +26,6 @@ git clone https://github.com/MineCrossing/Frontend.git && cd Frontend/ && npm in
 cd ..
 git clone https://github.com/MineCrossing/Store.git # Seperate PHP Laravel Store
 git clone https://github.com/MineCrossing/NookCore.git # Minecraft Bukkit Plugin
+echo "Finished grabbing repositories!"
 
 echo "Setup finished! Good luck have fun!"
